8a61f1b49e39a8a0cb33e6a843adf3d84e567858
Your Name <alo20172@uvg.edu.gt>
2022-03-31 11:05:42 -0600

    Factory with ilisst and stack immplemented

---
+++ HDT4REPO/src/calculator/Factory.java
@@ -0,0 +1,62 @@
+package calculator;
+import java.util.Scanner;
+public class Factory {
+	
+	/**
+	 * Creates a stack based on the option, returns null if its not valid
+	 * @param selectedFactory
+	 * @return IStack<String> based on the option
+	 * @exception null
+	 */
+	public static IStack<String> CreateStack(String selectedFactory) {
+		switch (selectedFactory) {
+		
+		case "Vector":{
+			System.out.print("\nSe ha creado un vector");
+			return new vector<>();
+		}
+		case "ArrayList":{
+			System.out.print("\nSe ha creado un arrayList");
+			return new arrayList<>();
+		}
+		case "List":{
+			IList<String> list = CreateList();
+			return (IStack<String>)list;
+		}
+		default:{
+			System.out.print("\nNo ha ingresado una opcion valida");
+			return null;
+		}
+		
+		}
+		
+	}
+	/**
+	 * Creates a stack based on which kind of list
+	 * @return
+	 */
+	private static IList<String> CreateList(){
+		Scanner scan = new Scanner(System.in);
+		System.out.print("\nIngrese el tipo de lista (LinkedList, DoubleLinkedList): ");
+		String selectedList = scan.nextLine();
+		switch(selectedList) {
+		
+		case "LinkedList": {
+			scan.close();
+			return new linkedList<>();
+			
+		}
+		case "DoubleLinkedList" :{
+			scan.close();
+			return new doubleLinkedList<>();
+			
+		}
+		default:{
+			scan.close();
+			System.out.print("\nNo es una opcion valida");
+			return null;
+		}
+		
+		}
+	}
+}
