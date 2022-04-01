29fdcd4bd6f029bece51d6f62dd49d1c85a9b154
Your Name <alo20172@uvg.edu.gt>
2022-03-31 18:01:11 -0600

    Javadoc 4 the calculator

--- HDT4REPO/src/calculator/Calculator.java
+++ HDT4REPO/src/calculator/Calculator.java
@@ -3,8 +3,14 @@ package calculator;
 public class Calculator {
 	
 	private static volatile Calculator instance = null;
+	/**
+	 * The calculator constructor is private because in this case it will not create more instances of calculator
+	 */
 	private Calculator() {}
-	
+	/**
+	 * Is the SINGLETON method to create the calculator
+	 * @return calculator, if it is already created it returns the same calculator
+	 */
 	public static Calculator getInstance() {
 		if (instance == null) {
 			synchronized(Calculator.class) {
@@ -17,7 +23,12 @@ public class Calculator {
 	}
 	
 	
-	
+	/**
+	 * It is the method to do sums
+	 * @param value2
+	 * @param value1
+	 * @return result as a string in case it is valid to do the operation.
+	 */
 	public String add(String value2, String value1) {
 		try{
 			String result = String.valueOf(Float.parseFloat(value1)+Float.parseFloat(value2));
@@ -28,7 +39,12 @@ public class Calculator {
 			return null;
 		}
 	}
-	
+	/**
+	 * It is the method to do subs
+	 * @param value2
+	 * @param value1
+	 * @return result as a string in case it is valid to do the operation.
+	 */
 	public String subs(String value2, String value1) {
 		try{
 			String result = String.valueOf(Float.parseFloat(value1)-Float.parseFloat(value2));
@@ -39,7 +55,12 @@ public class Calculator {
 			return null;
 		}
 	}
-	
+	/**
+	 * It is the method to do multiplication
+	 * @param value2
+	 * @param value1
+	 * @return result as a string in case it is valid to do the operation.
+	 */
 	public String mult(String value2, String value1) {
 		try{
 			String result = String.valueOf(Float.parseFloat(value1)*Float.parseFloat(value2));
@@ -50,7 +71,12 @@ public class Calculator {
 			return null;
 		}
 	}
-	
+	/**
+	 * It is the method to do divisions
+	 * @param value2
+	 * @param value1
+	 * @return result as a string in case it is valid to do the operation.
+	 */
 	public String div(String value2, String value1) {
 		try{
 			String result = String.valueOf(Float.parseFloat(value1)/Float.parseFloat(value2));
@@ -61,7 +87,12 @@ public class Calculator {
 			return null;
 		}
 	}
-	
+	/**
+	 * It is the method to do exponentials
+	 * @param value2
+	 * @param value1
+	 * @return result as a string in case it is valid to do the operation.
+	 */
 	public String exp(String value2, String value1) {
 		try{
 			String result = String.valueOf(Math.pow(Double.parseDouble(value1), Double.parseDouble(value2)));
